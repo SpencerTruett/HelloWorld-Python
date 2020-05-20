d58c14a1eb5ed17b7216be52c3c1d9a80b29e0d8
Spencer Truett <srtruett1993@gmail.com>
2020-05-20 15:30:02 -0500

    first commit

---
+++ HelloWorld_Python.py
@@ -0,0 +1,16 @@
+print("This line will be printed.")
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+print("Hello, World!")
+
+stringOne = "Hello, "
+stringTwo = "World!"
+stringThree = " This is pretty cool!"
+
+string = stringOne + stringTwo + stringThree
+
+print(string)
